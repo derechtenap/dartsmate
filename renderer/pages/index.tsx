1ef58dd2a30d6c2cc8974507ea09c3b7d87f4307
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2022-11-17 23:39:13 +0100

    Add SideNavigation

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,7 +1,17 @@
 import { NextPage } from "next";
+import SideNavigation from "../components/SideNavigation";
 
 const menuPage: NextPage = () => {
-  return <>menuPage</>;
+  return (
+    <main className="grid grid-cols-6 gap-4 w-screen h-screen">
+      <aside className="col-span-1 bg-gray-800">
+        <SideNavigation />
+      </aside>
+      <section className="col-span-5 flex justify-center items-center h-full text-gray-800">
+        <p className="text-6xl italic">DartMate</p>
+      </section>
+    </main>
+  );
 };
 
 export default menuPage;
