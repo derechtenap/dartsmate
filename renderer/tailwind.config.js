c01e97cd50db2fe245198027d677e4f43d4b72dc
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-02-09 15:41:54 +0100

    Add `tailwindcss` + `daisyui`

---
+++ renderer/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
+    "./renderer/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#264574",
+          secondary: "#f4f0d4",
+          accent: "#1FB2A5",
+          neutral: "#ebeaec",
+          "base-100": "#23252b",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
