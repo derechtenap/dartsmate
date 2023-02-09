6db66bf8a857bd6644590fdd80c0ae1a1048c473
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-02-09 15:42:48 +0100

    Removed react-bootstrap javascript

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
 import type { AppProps } from "next/app";
 
-import "../styles/main.scss";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  // Fix: Server Error `ReferenceError: document is not defined`
-  // Wait until the DOM is ready and then require the bootstrap js
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
   return <Component {...pageProps} />;
 };
 
