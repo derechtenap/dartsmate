5f08d09f5a13ddd243307f6540cf25dee9c900e0
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-02-10 23:53:00 +0100

    Add scrollbar styling

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
+import "../styles/scrollbar.css";
 import "@splidejs/react-splide/css";
 
 const App = ({ Component, pageProps }: AppProps) => {
