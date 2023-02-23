a22cdbd539bc0415f58289d2d4d89b9713bb9973
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-02-23 16:21:55 +0100

    Removed `margin-top`

--- renderer/components/navs/optionsMenu/OptionsList.tsx
+++ renderer/components/navs/optionsMenu/OptionsList.tsx
@@ -3,17 +3,17 @@ import { optionsNavLinks as links } from "utils/ui/navigation";
 
 const List = () => {
   return (
-    <nav className="mt-4 flex justify-center text-xl">
-    <ul className="cursor-pointer text-primary-content">
-      {links.map((link) => (
-        <Link href={link.href} key={link.name}>
-          <li className="p-4 transition-all hover:scale-125 hover:bg-primary hover:text-white">
-            {link.icon}
-          </li>
-        </Link>
-      ))}
-    </ul>
-  </nav>
+    <nav className="flex justify-center text-xl">
+      <ul className="cursor-pointer text-primary-content">
+        {links.map((link) => (
+          <Link href={link.href} key={link.name}>
+            <li className="p-4 transition-all hover:scale-125 hover:bg-primary hover:text-white">
+              {link.icon}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
