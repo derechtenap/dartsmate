c474720d539949a2a13a385293d29a02a8cb0418
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-02-25 00:27:35 +0100

    Add `ProfileFile` type

--- renderer/global.d.ts
+++ renderer/global.d.ts
@@ -1,7 +1,19 @@
 export {};
 
 declare global {
+  type ProfileFile = {
     name: string;
     uuid: string;
+    avatarImage?: string; // Base64 string
+    createdAt: number; // Number of milliseconds since January 1, 1970
+    updatedAt: number;
+    stats: {
+      games: number; // Played games
+      wins: number;
+      avg: number;
+      throws: number;
+      totalScore: number;
+      missedThrows: number;
+    };
   };
 }
