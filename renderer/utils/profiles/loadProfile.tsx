dc7b377763b06d503f16bd58317c762a27c5d0d5
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-02-24 19:21:55 +0100

    Fix: Argument of type `Buffer` is not assignable to parameter of type `string`
    Also removed unnecessary `await`

--- renderer/utils/profiles/loadProfile.tsx
+++ renderer/utils/profiles/loadProfile.tsx
@@ -2,7 +2,10 @@ import { readFileSync } from "fs";
 import { profileDir } from "./profileFolderHandling";
 
 export const loadProfile = async (filename: string) => {
-  const profile = await readFileSync(`${profileDir}/${filename}`);
+  const profile = readFileSync(`${profileDir}/${filename}`, {
+    encoding: "utf-8",
+    flag: "r",
+  });
 
   return await JSON.parse(profile);
 };
