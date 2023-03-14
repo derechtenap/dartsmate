d084ba0884a9035cde18970867cb7f94aae9930c
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-03-14 13:02:15 +0100

    Add `tanstack react-query`

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,11 +1,19 @@
 import type { AppProps } from "next/app";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "../styles/globals.css";
 import "../styles/scrollbar.css";
 import "@splidejs/react-splide/css";
 
+const queryClient = new QueryClient();
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
