8218fac69606f8aa6d22778d6f151c86677ecb29
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-04-14 17:22:04 +0200

    Updated `Round` ts type and added `Throw` type

--- renderer/global.d.ts
+++ renderer/global.d.ts
@@ -6,16 +6,18 @@ declare global {
 
   type Round = {
     elapsed_throwing_time: number; // In seconds
-    throws: Array<{
-      zone: number;
-      points: number;
-      round_avg: number;
-      is_double: boolean;
-      is_triple: boolean;
-      is_outer_bull: boolean;
-      is_bullseye: boolean;
-      is_missed: boolean;
-    }>;
+    throws: Array<Throw>;
+  };
+
+  type Throw = {
+    zone: number;
+    points: number;
+    round_avg: number;
+    is_double: boolean;
+    is_triple: boolean;
+    is_outer_bull: boolean;
+    is_bullseye: boolean;
+    is_missed: boolean;
   };
 
   type ProfileFile = {
