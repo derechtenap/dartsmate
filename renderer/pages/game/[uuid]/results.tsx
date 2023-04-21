aaa7297444fbb716fbd1f98ebef7e2deb51e9775
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-04-21 15:55:22 +0200

    Add `Back to Index` btn for testing purposes

--- renderer/pages/game/[uuid]/results.tsx
+++ renderer/pages/game/[uuid]/results.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from "next";
+import Button from "@/components/Button";
+import { useRouter } from "next/router";
 
 const GameResults: NextPage = () => {
-  return <>SUMMARY!</>;
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>Results</h1>
+      <Button action={() => router.push("/")} styles="btn">
+        Back To Index Page
+      </Button>
+    </>
+  );
 };
 
 export default GameResults;
