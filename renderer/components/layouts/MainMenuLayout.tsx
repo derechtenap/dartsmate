ea4a854e98e915dc81ee464079e39fb88bf6cdd2
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-04-26 14:35:42 +0200

    Replaced hardcoded app name with the utils constants value

--- renderer/components/layouts/MainMenuLayout.tsx
+++ renderer/components/layouts/MainMenuLayout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { APP_NAME, APP_VERSION } from "utils/constants";
 import OptionsList from "../navs/optionsMenu/OptionsList";
 
 type Props = {
@@ -7,8 +8,6 @@ type Props = {
 };
 
 const DefaultLayout = ({ children, title }: Props) => {
-  const appVersion: string = require("../../../package.json").version;
-
   return (
     <>
       <Head>
@@ -18,7 +17,7 @@ const DefaultLayout = ({ children, title }: Props) => {
       <main className="flex h-screen w-screen">
         <aside className="h-full w-16 flex-none bg-base-300 text-white">
           <p className="absolute bottom-16 w-16 -rotate-90 transform whitespace-nowrap opacity-80">
-            DartMate &mdash; {appVersion}
+            {APP_NAME} &mdash; {APP_VERSION}
           </p>
           <OptionsList />
         </aside>
