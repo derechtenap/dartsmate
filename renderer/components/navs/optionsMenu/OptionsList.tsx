5693388414c117a4d3bd4ee347a8e97936da40ca
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-05-03 13:34:44 +0200

    Fix: Property 'href' does not exist on type 'never'

--- renderer/components/navs/optionsMenu/OptionsList.tsx
+++ renderer/components/navs/optionsMenu/OptionsList.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 import { optionsNavLinks as links } from "utils/ui/navigation";
 
 const List = () => {
   return (
     <nav className="flex justify-center text-xl">
       <ul className="cursor-pointer text-primary-content">
-        {links.map((link) => (
+        {links.map((link: { href: string; name: string; icon: ReactNode }) => (
           <Link href={link.href} key={link.name}>
             <li className="p-4 transition-all hover:scale-125 hover:bg-primary hover:text-white">
               {link.icon}
