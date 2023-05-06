133ccc04e7f6a9f771ac26a1dc23a21958fd838e
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-05-06 15:40:33 +0200

    Fix `data` is possibly `undefined`

--- renderer/pages/lobby.tsx
+++ renderer/pages/lobby.tsx
@@ -40,7 +40,7 @@ const Lobby: NextPage = () => {
   };
 
   const [backgroundBlur, setBackgroundBlur] = useState<boolean>(false);
-  const [selectedPlayers, setSelectedPlayers] = useState<any[]>([]);
+  const [selectedPlayers, setSelectedPlayers] = useState<ProfileFile[]>([]);
 
   const handleGameStart = () => {
     const lobbySettings: GameFile = {
@@ -59,7 +59,7 @@ const Lobby: NextPage = () => {
       })),
       uuid: crypto.randomUUID(),
       created_at: Date.now(),
-      app_version: APP_VERSION,
+      app_version: APP_VERSION as string,
       current_player: selectedPlayers[0].uuid,
       game_log: [
         {
@@ -86,7 +86,11 @@ const Lobby: NextPage = () => {
 
     if (selectedPlayers.length >= GAME_MAX_PLAYERS) {
       toast.error(
-        `The lobby is full! You reached the maximum player capacity of ${GAME_MAX_PLAYERS}. Please remove a player, before you can add ${player.name} as a player.`,
+        `The lobby is full! You reached the maximum player capacity of ${
+          GAME_MAX_PLAYERS as number
+        }. Please remove a player, before you can add ${
+          player.name
+        } as a player.`,
         {
           position: toast.POSITION.TOP_RIGHT,
           theme: "dark",
@@ -104,108 +108,109 @@ const Lobby: NextPage = () => {
 
   if (isLoading) return <>Loading..</>;
 
-  return (
-    <SidebarLayout title="Lobby">
-      <div className="drawer">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div
-          className={`drawer-content flex flex-col ${
-            backgroundBlur ? "blur" : ""
-          }`}
-        >
-          <header className="w-full bg-base-200 p-4">
-            <h1 className="text-white">Lobby</h1>
-            <p className="mb-0">
-              Please select all players who want to participate in the game. You
-              can select up to {GAME_MAX_PLAYERS} players. Click on a player
-              profile to add them the new game.
-            </p>
-          </header>
+  if (data)
+    return (
+      <SidebarLayout title="Lobby">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div
+            className={`drawer-content flex flex-col ${
+              backgroundBlur ? "blur" : ""
+            }`}
+          >
+            <header className="w-full bg-base-200 p-4">
+              <h1 className="text-white">Lobby</h1>
+              <p className="mb-0">
+                Please select all players who want to participate in the game.
+                You can select up to {GAME_MAX_PLAYERS} players. Click on a
+                player profile to add them the new game.
+              </p>
+            </header>
 
-          <ul className="menu menu-horizontal w-full bg-base-300 text-white">
-            <li>
-              <label
-                htmlFor="my-drawer"
-                className="btn-ghost btn rounded-none"
-                onClick={() => setBackgroundBlur(!backgroundBlur)}
-              >
-                <HiUserGroup /> Players
-              </label>
-            </li>
-            <li className="mr-8 flex-1 items-center justify-end bg-base-200 font-mono text-xs uppercase">
-              Players: {selectedPlayers.length} / {GAME_MAX_PLAYERS}{" "}
-            </li>
-            <li className="ml-auto">
-              <button className="btn-disabled btn" disabled>
-                <HiDotsHorizontal />
-              </button>
-            </li>
-          </ul>
-
-          <div className="flex w-full flex-1 items-start">
-            <section className="grid w-full grid-cols-2 xl:grid-cols-4 xl:gap-y-16">
-              {selectedPlayers.map((player) => (
-                <div
-                  className="card inline-flex items-center justify-center gap-y-4 rounded-none p-4 text-center transition-all hover:bg-base-300 xl:text-xl xl:font-bold"
-                  key={player.uuid}
+            <ul className="menu menu-horizontal w-full bg-base-300 text-white">
+              <li>
+                <label
+                  htmlFor="my-drawer"
+                  className="btn-ghost btn rounded-none"
+                  onClick={() => setBackgroundBlur(!backgroundBlur)}
                 >
-                  <Avatar
-                    imgSrc={player.avatar_image}
-                    name={player.name}
-                    size="w-16 xl:w-32"
-                  />
-                  {player.name}
-                </div>
-              ))}
-            </section>
-            <aside className="flex w-96 flex-col justify-between self-stretch overflow-hidden bg-base-200">
-              <main className="p-4">
-                <p className="flex items-center gap-4 text-xl font-bold">
-                  <HiCog className="text-3xl hover:animate-spin" />
-                  Settings
-                </p>
-                <form onChange={handleSubmit(onSubmit)}>
-                  <div className="form-control">
-                    <label className="label" htmlFor="score-mode">
-                      <span className="label-text">Score Mode</span>
-                    </label>
-                    <select
-                      className="select w-full max-w-xs px-4"
-                      id="score-mode"
-                      {...register("scoreMode")}
-                    >
-                      {GAME_SCORE_MODES.map((mode) => (
-                        <option key={mode} value={mode}>
-                          {mode}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                      <span className="label-text">Sets</span>
-                    </label>
-                    <input
-                      type="number"
-                      className="input-bordered input w-full"
-                      disabled
-                      {...register("sets")}
-                      max={GAME_MAX_SETS}
+                  <HiUserGroup /> Players
+                </label>
+              </li>
+              <li className="mr-8 flex-1 items-center justify-end bg-base-200 font-mono text-xs uppercase">
+                Players: {selectedPlayers.length} / {GAME_MAX_PLAYERS}{" "}
+              </li>
+              <li className="ml-auto">
+                <button className="btn-disabled btn" disabled>
+                  <HiDotsHorizontal />
+                </button>
+              </li>
+            </ul>
+
+            <div className="flex w-full flex-1 items-start">
+              <section className="grid w-full grid-cols-2 xl:grid-cols-4 xl:gap-y-16">
+                {selectedPlayers.map((player) => (
+                  <div
+                    className="card inline-flex items-center justify-center gap-y-4 rounded-none p-4 text-center transition-all hover:bg-base-300 xl:text-xl xl:font-bold"
+                    key={player.uuid}
+                  >
+                    <Avatar
+                      imgSrc={player.avatar_image}
+                      name={player.name}
+                      size="w-16 xl:w-32"
                     />
+                    {player.name}
                   </div>
-                  <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                      <span className="label-text">Legs</span>
-                    </label>
-                    <input
-                      type="number"
-                      className="input-bordered input w-full"
-                      disabled
-                      {...register("legs")}
-                      max={GAME_MAX_LEGS}
-                    />
-                  </div>
-                  {/*
+                ))}
+              </section>
+              <aside className="flex w-96 flex-col justify-between self-stretch overflow-hidden bg-base-200">
+                <main className="p-4">
+                  <p className="flex items-center gap-4 text-xl font-bold">
+                    <HiCog className="text-3xl hover:animate-spin" />
+                    Settings
+                  </p>
+                  <form onChange={handleSubmit(onSubmit)}>
+                    <div className="form-control">
+                      <label className="label" htmlFor="score-mode">
+                        <span className="label-text">Score Mode</span>
+                      </label>
+                      <select
+                        className="select w-full max-w-xs px-4"
+                        id="score-mode"
+                        {...register("scoreMode")}
+                      >
+                        {GAME_SCORE_MODES.map((mode: number) => (
+                          <option key={mode} value={mode}>
+                            {mode}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                      <label className="label">
+                        <span className="label-text">Sets</span>
+                      </label>
+                      <input
+                        type="number"
+                        className="input-bordered input w-full"
+                        disabled
+                        {...register("sets")}
+                        max={GAME_MAX_SETS as number}
+                      />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                      <label className="label">
+                        <span className="label-text">Legs</span>
+                      </label>
+                      <input
+                        type="number"
+                        className="input-bordered input w-full"
+                        disabled
+                        {...register("legs")}
+                        max={GAME_MAX_LEGS as number}
+                      />
+                    </div>
+                    {/*
 
                   TODO: Currently disabled. Logic wont work properly!
                   The games will wont use a randomized order until this is
@@ -229,66 +234,72 @@ const Lobby: NextPage = () => {
 
                   </div>
                   */}
-                </form>
-              </main>
-              <button
-                className={`btn-primary btn h-32 w-full self-end rounded-none text-3xl ${
-                  selectedPlayers.length < 1 ? "btn-disabled" : "animate-pulse"
-                }`}
-                {...(selectedPlayers.length < 1 ? { disabled: true } : {})}
-                onClick={() => handleGameStart()}
-              >
-                Start Game
-              </button>
-            </aside>
-          </div>
-        </div>
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer"
-            className="drawer-overlay"
-            onClick={() => setBackgroundBlur(!backgroundBlur)}
-          ></label>
-          <ul className="menu w-80 bg-base-100 p-4 text-base-content">
-            <li>
-              <Button
-                action={() => router.push("/profiles/createProfile?from=lobby")}
-                color="ghost"
-              >
-                Create A New Profile
-              </Button>
-              <button disabled className="btn-disabled btn-ghost btn mt-2">
-                Add Guest
-              </button>
-            </li>
-
-            <li className="cursor-default border-b border-primary bg-transparent text-sm"></li>
-            {data.map((player) => (
-              <li className="flex flex-row" key={player.uuid}>
-                <span
-                  className="flex-1"
-                  onClick={() => handlePlayerSelection(player)}
-                >
-                  <Avatar imgSrc={player.avatar_image} name={player.name} />
-                  {player.name}
-                </span>
+                  </form>
+                </main>
                 <button
-                  className="text-2xl"
-                  onClick={() => handlePlayerSelection(player)}
+                  className={`btn-primary btn h-32 w-full self-end rounded-none text-3xl ${
+                    selectedPlayers.length < 1
+                      ? "btn-disabled"
+                      : "animate-pulse"
+                  }`}
+                  {...(selectedPlayers.length < 1 ? { disabled: true } : {})}
+                  onClick={() => handleGameStart()}
                 >
-                  {selectedPlayers.includes(player) ? (
-                    <HiUserRemove />
-                  ) : (
-                    <HiUserAdd />
-                  )}
+                  Start Game
+                </button>
+              </aside>
+            </div>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              className="drawer-overlay"
+              onClick={() => setBackgroundBlur(!backgroundBlur)}
+            ></label>
+            <ul className="menu w-80 bg-base-100 p-4 text-base-content">
+              <li>
+                <Button
+                  action={() =>
+                    router.push("/profiles/createProfile?from=lobby")
+                  }
+                  color="ghost"
+                >
+                  Create A New Profile
+                </Button>
+                <button disabled className="btn-disabled btn-ghost btn mt-2">
+                  Add Guest
                 </button>
               </li>
-            ))}
-          </ul>
+
+              <li className="cursor-default border-b border-primary bg-transparent text-sm"></li>
+              {data.map((player: ProfileFile) => (
+                <li className="flex flex-row" key={player.uuid}>
+                  <span
+                    className="flex-1"
+                    onClick={() => handlePlayerSelection(player)}
+                  >
+                    <Avatar imgSrc={player.avatar_image} name={player.name} />
+                    {player.name}
+                  </span>
+                  <button
+                    className="text-2xl"
+                    onClick={() => handlePlayerSelection(player)}
+                  >
+                    {selectedPlayers.includes(player) ? (
+                      <HiUserRemove />
+                    ) : (
+                      <HiUserAdd />
+                    )}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    </SidebarLayout>
-  );
+      </SidebarLayout>
+    );
+
+  return <></>;
 };
 
 export default Lobby;
