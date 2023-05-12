169afabd37396cad3552a437f5412b787b3e81ae
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-05-12 15:08:40 +0200

    Fix: 3 linting errors
       7:49  error  Async arrow function 'loadProfile' has no 'await' expression              @typescript-eslint/require-await
       9:35  error  Unsafe argument of type `any` assigned to a parameter of type `string`    @typescript-eslint/no-unsafe-argument
      25:22  error  Unsafe argument of type `any` assigned to a parameter of type `PathLike`  @typescript-eslint/no-unsafe-argument

--- renderer/utils/profiles/load.ts
+++ renderer/utils/profiles/load.ts
@@ -1,19 +1,18 @@
 import { readdirSync, readFileSync } from "fs";
 import path from "path";
-
 import { PROFILE_SAVE_DIRECTORY } from "utils/constants";
 
 // Loads the file content of a specific profile using the profile uuid
-export const loadProfile = (file: string) => {
+export const loadProfile = async (file: string) => {
   try {
-    const profilePath = path.join(PROFILE_SAVE_DIRECTORY, file);
+    const profilePath = path.join(PROFILE_SAVE_DIRECTORY as string, file);
 
     const profile = readFileSync(profilePath, {
       encoding: "utf-8",
       flag: "r",
     });
 
-    const profileJSON = JSON.parse(profile) as ProfileFile;
+    const profileJSON = (await JSON.parse(profile)) as ProfileFile;
     return profileJSON;
   } catch (err) {
     throw new Error(`Failed to load profile "${file}"!`);
@@ -22,7 +21,7 @@ export const loadProfile = (file: string) => {
 
 // Returns a array with each file in the profile directory
 export const readProfileDir = () => {
-  return readdirSync(PROFILE_SAVE_DIRECTORY, {
+  return readdirSync(PROFILE_SAVE_DIRECTORY as string, {
     encoding: "utf-8",
   });
 };
