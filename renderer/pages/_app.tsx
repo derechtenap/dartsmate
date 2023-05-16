bcbd9990cbbbb75d5deeee7b6b8e3c532fe21682
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-05-16 15:38:04 +0200

    Add `mantine` to the application

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,21 +1,39 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import { MantineProvider } from "@mantine/core";
+
+import { APP_NAME } from "utils/constants";
 
 import "../styles/globals.css";
 import "../styles/scrollbar.css";
 import "@splidejs/react-splide/css";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
+const appName = APP_NAME;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ToastContainer />
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>{appName}</title>
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <ToastContainer />
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </MantineProvider>
+    </>
   );
 };
 
