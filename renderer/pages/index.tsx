b3c64e1f35a96ebc115e6ca46a8fbff92e844ae1
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-05-16 13:57:53 +0200

    Add `Options` type

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -7,6 +7,7 @@ import NewGameCta from "@/components/slides/NewGameCta";
 import {
   Splide as Carousel,
   SplideSlide as Slide,
+  Options,
 } from "@splidejs/react-splide";
 
 import { mainMenuSlidesOptions } from "utils/ui/SlideOptions";
@@ -17,7 +18,7 @@ const IndexPage: NextPage = () => {
       <Carousel
         aria-label="Menu"
         className="h-4/6 w-full"
-        options={mainMenuSlidesOptions}
+        options={mainMenuSlidesOptions as Options}
       >
         <Slide>
           <NewGameCta />
