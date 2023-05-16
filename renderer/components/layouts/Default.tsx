ec473cdfd8fabd5a794a60c38514652e15630eee
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-05-16 17:57:01 +0200

    Add settings menu to the `Navbar`

--- renderer/components/layouts/Default.tsx
+++ renderer/components/layouts/Default.tsx
@@ -1,20 +1,74 @@
-import { AppShell, Navbar } from "@mantine/core";
+import { AppShell, Center, Menu, Navbar, Text, ThemeIcon } from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import {
+  IconLanguage,
+  IconMoon,
+  IconSettings,
+  IconSun,
+} from "@tabler/icons-react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const DefaultLayout = ({ children }: Props) => {
+  const [colorScheme, setColorScheme] = useLocalStorage({
+    key: "mantine-color-scheme",
+  });
+
   return (
     <AppShell
       padding="md"
       navbar={
-        <Navbar height="100vh" p="xs" width={{ base: 150 }}>
+        <Navbar height="100vh" p="xs" width={{ base: 100 }}>
           <Navbar.Section>TOP</Navbar.Section>
           <Navbar.Section grow mt="lg">
             MID
           </Navbar.Section>
-          <Navbar.Section>FOOTER</Navbar.Section>
+          <Navbar.Section>
+            <Center>
+              <Menu arrowPosition="center" position="right-end" withArrow>
+                <Menu.Target>
+                  <ThemeIcon
+                    variant="default"
+                    size="xl"
+                    className="cursor-pointer"
+                  >
+                    <IconSettings />
+                  </ThemeIcon>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Label>Settings</Menu.Label>
+                  <Menu.Item
+                    onClick={() =>
+                      setColorScheme(colorScheme === "dark" ? "light" : "dark")
+                    }
+                    closeMenuOnClick={false}
+                    icon={colorScheme === "light" ? <IconSun /> : <IconMoon />}
+                    rightSection={
+                      <Text size="xs" color="dimmed" ml="xs">
+                        CTRL + T
+                      </Text>
+                    }
+                  >
+                    Color Scheme
+                  </Menu.Item>
+                  <Menu.Item
+                    closeMenuOnClick={false}
+                    icon={<IconLanguage />}
+                    rightSection={
+                      <Text size="xs" color="dimmed" ml="xs">
+                        CTRL + L
+                      </Text>
+                    }
+                    disabled
+                  >
+                    Language
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Center>
+          </Navbar.Section>
         </Navbar>
       }
     >
