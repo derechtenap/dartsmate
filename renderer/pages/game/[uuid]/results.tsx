fe0c03434b1256a3154b2774e8816b91bc8e84d6
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-06-05 12:30:12 +0200

    Removed old page styling

--- renderer/pages/game/[uuid]/results.tsx
+++ renderer/pages/game/[uuid]/results.tsx
@@ -1,16 +1,9 @@
 import { NextPage } from "next";
-import Button from "@/components/Button";
-import { useRouter } from "next/router";
 
 const GameResults: NextPage = () => {
-  const router = useRouter();
-
   return (
     <>
       <h1>Results</h1>
-      <Button action={() => router.push("/")} styles="btn">
-        Back To Index Page
-      </Button>
     </>
   );
 };
