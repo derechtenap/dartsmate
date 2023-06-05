fae524a141ad31f2a5772ea479b3ba4b7ecb8621
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-06-05 12:31:05 +0200

    Add carousel

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,8 +1,49 @@
 import { NextPage } from "next";
 import DefaultLayout from "@/components/layouts/Default";
 
+import { Carousel } from "@mantine/carousel";
+import { Center, Text, Title } from "@mantine/core";
+
+import { APP_NAME } from "utils/constants";
+
 const IndexPage: NextPage = () => {
-  return <DefaultLayout>Content Here</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <div style={{ height: "100%", display: "flex", alignContent: "center" }}>
+        <Carousel
+          withControls={false}
+          withIndicators
+          height="100%"
+          sx={{ flex: 1 }}
+          loop
+          orientation="vertical"
+        >
+          <Carousel.Slide>
+            <Center
+              h="100%"
+              sx={{
+                flexDirection: "column",
+                alignItems: "flex-start",
+                gap: "2rem",
+              }}
+              p="xl"
+            >
+              <Title
+                size="3rem"
+                weight={900}
+                sx={{ textTransform: "uppercase" }}
+              >
+                Welcome to {APP_NAME}
+              </Title>
+              <Text weight={600} fz="1.5rem">
+                Lets start a new game!
+              </Text>
+            </Center>
+          </Carousel.Slide>
+        </Carousel>
+      </div>
+    </DefaultLayout>
+  );
 };
 
 export default IndexPage;
