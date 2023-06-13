3f75451445cef6644afb57e9464624a8d1b945cd
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-06-13 11:14:46 +0200

    Rebuild navbar

--- renderer/components/layouts/Default.tsx
+++ renderer/components/layouts/Default.tsx
@@ -1,26 +1,23 @@
 import {
-  ActionIcon,
   AppShell,
-  Center,
-  Menu,
+  createStyles,
   Navbar,
-  NavLink,
-  Text,
+  rem,
+  Stack,
+  Tooltip,
+  UnstyledButton,
 } from "@mantine/core";
-import { useFullscreen, useLocalStorage } from "@mantine/hooks";
 import {
-  IconAdjustments,
-  IconCategory,
-  IconCirclePlus,
-  IconCircleX,
-  IconMaximize,
-  IconMaximizeOff,
-  IconMoon,
+  Icon,
+  IconDisc,
+  IconHome2,
+  IconListNumbers,
+  IconSchool,
   IconSettings,
-  IconSun,
-  IconUserCircle,
+  IconSquareRoundedX,
+  IconTournament,
+  IconUsersGroup,
 } from "@tabler/icons-react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 
@@ -28,98 +25,136 @@ type Props = {
   children: React.ReactNode;
 };
 
-export const navbarWidth = 160;
+type NavbarLinkProps = {
+  active?: boolean;
+  disabled?: boolean;
+  icon: Icon;
+  label: string;
+  route: string;
+};
+
+export const navbarWidth = 70;
 
 const DefaultLayout = ({ children }: Props) => {
-  const router = useRouter();
+  const { route, push } = useRouter();
   const { t } = useTranslation(["common"]);
 
-  const [colorScheme, setColorScheme] = useLocalStorage({
-    key: "mantine-color-scheme",
-  });
-
-  const { toggle, fullscreen } = useFullscreen();
-
-  const toggleFullscreen = () => {
-    return toggle();
-  };
-
-  const routes = [
+  const mainRoutes: NavbarLinkProps[] = [
+    { icon: IconHome2, label: t("navbar.routes.homeLabel"), route: "/" },
+    { icon: IconDisc, label: t("navbar.routes.newGameLabel"), route: "/lobby" },
     {
-      icon: <IconCategory />,
-      label: t("navbar.routes.routeHubLabel"),
-      link: "/",
+      icon: IconSchool,
+      disabled: true,
+      label: t("navbar.routes.trainingLabel"),
+      route: "/training",
     },
     {
-      icon: <IconCirclePlus />,
-      label: t("navbar.routes.routeNewGameLabel"),
-      link: "/lobby",
+      icon: IconTournament,
+      disabled: true,
+      label: t("navbar.routes.tournamentLabel"),
+      route: "/tournament",
     },
     {
-      icon: <IconUserCircle />,
-      label: t("navbar.routes.routeProfilesLabel"),
-      link: "/profiles",
+      icon: IconUsersGroup,
+      label: t("navbar.routes.profilesLabel"),
+      route: "/profiles",
     },
     {
-      icon: <IconCircleX />,
-      label: t("navbar.routes.routeCloseAppLabel"),
-      link: "/quit",
+      icon: IconListNumbers,
+      disabled: true,
+      label: t("navbar.routes.rankingLabel"),
+      route: "/ranking",
     },
   ];
+
+  const miscRoutes: NavbarLinkProps[] = [
+    {
+      icon: IconSettings,
+      label: t("navbar.routes.settingsLabel"),
+      route: "/settings",
+    },
+    {
+      icon: IconSquareRoundedX,
+      label: t("navbar.routes.quitAppLabel"),
+      route: "#?quitApp",
+    },
+  ];
+
+  const useStyles = createStyles((theme) => ({
+    link: {
+      width: rem(50),
+      height: rem(50),
+      borderRadius: theme.radius.md,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[0]
+          : theme.colors.dark[5],
+
+      "&:hover": {
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[5]
+            : theme.colors.gray[1],
+      },
+      "&:disabled": {
+        cursor: "not-allowed",
+      },
+    },
+    active: {
+      "&, &:hover": {
+        backgroundColor: theme.fn.variant({
+          variant: "light",
+          color: theme.primaryColor,
+        }).background,
+        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+          .color,
+      },
+    },
+  }));
+
+  const NavbarLink = ({
+    icon: Icon,
+    label,
+    disabled,
+    active,
+    route,
+  }: NavbarLinkProps) => {
+    const { classes, cx } = useStyles();
+    return (
+      <Tooltip label={label} position="right" offset={15} withArrow>
+        <UnstyledButton
+          onClick={() => void push(route)}
+          className={cx(classes.link, { [classes.active]: active })}
+          disabled={disabled}
+        >
+          <Icon size="1.2rem" stroke={1.5} />
+        </UnstyledButton>
+      </Tooltip>
+    );
+  };
+
+  const appendNavbarRoutes = (links: NavbarLinkProps[]) => {
+    return links.map((link) => (
+      <NavbarLink {...link} active={route === link.route} key={link.label} />
+    ));
+  };
 
   return (
     <AppShell
       navbar={
         <Navbar height="100vh" p="xs" width={{ base: navbarWidth }}>
           <Navbar.Section grow>
-            {routes.map((route) => (
-              <Link href={route.link} key={route.label}>
-                <NavLink label={route.label} icon={route.icon} />
-              </Link>
-            ))}
+            <Stack justify="center" spacing="xs">
+              {appendNavbarRoutes(mainRoutes)}
+            </Stack>
           </Navbar.Section>
           <Navbar.Section>
-            <Center>
-              <Menu arrowPosition="center" position="right-end" withArrow>
-                <Menu.Target>
-                  <ActionIcon size="xl">
-                    <IconSettings />
-                  </ActionIcon>
-                </Menu.Target>
-                <Menu.Dropdown>
-                  <Menu.Item
-                    onClick={() =>
-                      setColorScheme(colorScheme === "dark" ? "light" : "dark")
-                    }
-                    closeMenuOnClick={false}
-                    icon={colorScheme === "light" ? <IconSun /> : <IconMoon />}
-                    rightSection={
-                      <Text size="xs" color="dimmed" ml="xs">
-                        {t("navbar.menuItemColorSchemeLabelHotkey")}
-                      </Text>
-                    }
-                  >
-                    {t("navbar.menuItemColorSchemeLabel")}
-                  </Menu.Item>
-                  <Menu.Item
-                    onClick={() => void toggleFullscreen()}
-                    closeMenuOnClick={false}
-                    icon={fullscreen ? <IconMaximizeOff /> : <IconMaximize />}
-                  >
-                    {fullscreen
-                      ? t("navbar.menuItemToggleFullscreenModeWindowedLabel")
-                      : t("navbar.menuItemToggleFullscreenModeFullscreenLabel")}
-                  </Menu.Item>
-                  <Menu.Item
-                    closeMenuOnClick={false}
-                    icon={<IconAdjustments />}
-                    onClick={() => void router.push("/settings")}
-                  >
-                    {t("navbar.menuItemAppSettingsLabel")}
-                  </Menu.Item>
-                </Menu.Dropdown>
-              </Menu>
-            </Center>
+            <Stack justify="center" spacing="xs">
+              {appendNavbarRoutes(miscRoutes)}
+            </Stack>
           </Navbar.Section>
         </Navbar>
       }
