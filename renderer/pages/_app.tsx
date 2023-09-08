ecd5987e4401b24ebce7fed8941897d0b727bf69
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-09-08 16:54:27 +0200

    Add player selection inside the lobby

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -16,6 +16,7 @@ import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
 import { QueryClient } from "@tanstack/query-core";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { DehydratedState } from "@tanstack/react-query";
+import { Notifications } from "@mantine/notifications";
 
 const App = ({
   Component,
@@ -62,6 +63,7 @@ const App = ({
               colorScheme,
             }}
           >
+            <Notifications limit={5} position="top-right" />
             <Hydrate state={pageProps.dehydratedState}>
               <Component {...pageProps} />
             </Hydrate>
--- renderer/pages/lobby.tsx
+++ renderer/pages/lobby.tsx
@@ -10,26 +10,89 @@ import {
   Stack,
   Stepper,
   Text,
+  UnstyledButton,
 } from "@mantine/core";
 import { useState } from "react";
 import {
   IconAdjustmentsHorizontal,
+  IconCheck,
   IconTargetArrow,
   IconUsersPlus,
 } from "@tabler/icons-react";
 import { useProfiles } from "hooks/useProfiles";
 import { Profile } from "types/profile";
 import { getUsernameInitials } from "utils/misc/getUsernameInitials";
+import { notifications } from "@mantine/notifications";
 
 const LobbyPage: NextPage = () => {
   const { isLoading, isSuccess, data: profiles, refetch } = useProfiles();
-  const [matchPlayerList, setMatchPlayerList] = useState([]);
+  const [matchPlayerList, setMatchPlayerList] = useState<Profile[]>([]);
+
+  const handlePlayerSelection = (profile: Profile) => {
+    if (matchPlayerList.includes(profile)) {
+      // If the profile is already in the list and user clicks again on the avatar
+      // remove the profile from the player list
+      setMatchPlayerList((prev) => prev.filter((item) => item !== profile));
+      notifications.show({
+        color: "red",
+        title: `${profile.username} was removed from the next game!`,
+        message: "Click on the profile picture again to add them again.",
+      });
+    } else {
+      notifications.show({
+        title: `${profile.username} will be in the next game!`,
+        message: "Click on the profile picture again to remove them.",
+      });
+      setMatchPlayerList((prev) => [...prev, profile]);
+    }
+  };
+
   const steps = [
     {
       label: "Select Players",
       description: "Choose the Players for the Match",
       icon: <IconUsersPlus />,
-      content: <StepPlayers isSuccess={isSuccess} profiles={profiles} />,
+      content: (
+        <ScrollArea type="hover" h="420px">
+          <Grid mt="xl" mx="xs">
+            {isSuccess
+              ? profiles.map((profile) => (
+                  <Grid.Col span={2} key={profile.uuid}>
+                    <UnstyledButton
+                      display="block"
+                      mx="auto"
+                      p="md"
+                      onClick={() => handlePlayerSelection(profile)}
+                    >
+                      <Stack key={profile.uuid}>
+                        <Avatar mx="auto" size="lg" color={profile.color}>
+                          {matchPlayerList.includes(profile) ? (
+                            <IconCheck />
+                          ) : (
+                            getUsernameInitials(profile.username)
+                          )}
+                        </Avatar>
+                        <Text
+                          ta="center"
+                          tt="uppercase"
+                          fw="bold"
+                          color={
+                            matchPlayerList.includes(profile)
+                              ? "white"
+                              : "dimmed"
+                          }
+                          size="xs"
+                        >
+                          {profile.username}
+                        </Text>
+                      </Stack>
+                    </UnstyledButton>
+                  </Grid.Col>
+                ))
+              : null}
+          </Grid>
+        </ScrollArea>
+      ),
     },
     {
       label: "Configure Settings",
@@ -83,46 +146,15 @@ const LobbyPage: NextPage = () => {
           <Button onClick={() => prevStep()} disabled={active === 0}>
             Previous Step
           </Button>
-          <Button onClick={() => nextStep()} disabled={active === steps.length}>
+          <Button
+            onClick={() => nextStep()}
+            disabled={active === steps.length || matchPlayerList.length === 0}
+          >
             Next Step
           </Button>
         </Group>
       </PageHeader>
     </DefaultLayout>
-  );
-};
-
-type StepPlayersProps = {
-  isSuccess: boolean;
-  profiles: Profile[];
-};
-
-const StepPlayers = ({ isSuccess, profiles }: StepPlayersProps) => {
-  return (
-    <ScrollArea type="hover" h="420px">
-      <Grid mt="xl" mx="xs">
-        {isSuccess
-          ? profiles.map((profile) => (
-              <Grid.Col span={2} key={profile.uuid}>
-                <Stack key={profile.uuid} p="md">
-                  <Avatar mx="auto" size="lg" color={profile.color}>
-                    {getUsernameInitials(profile.username)}
-                  </Avatar>
-                  <Text
-                    ta="center"
-                    tt="uppercase"
-                    fw="bold"
-                    color="dimmed"
-                    size="xs"
-                  >
-                    {profile.username}
-                  </Text>
-                </Stack>
-              </Grid.Col>
-            ))
-          : null}
-      </Grid>
-    </ScrollArea>
   );
 };
 
