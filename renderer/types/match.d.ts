b1aa9657d36033f75302d4154d4d6957b0b192e5
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-09-16 13:44:22 +0200

    Add lobby settings to match type

--- renderer/types/match.d.ts
+++ renderer/types/match.d.ts
@@ -1,7 +1,17 @@
 import { UUID } from "crypto";
+import { Profile } from "./profile";
+
+declare type Checkout = "Any" | "Double" | "Single" | "Triple";
+declare type GameType = 901 | 701 | 501 | 301;
+declare type MatchStatus = "ABORTED" | "FINISHED" | "STARTED" | "UNFINISHED";
 
 declare type Match = {
   createdAt: number;
+  profiles: Profile[];
   updatedAt: number;
   uuid: UUID;
+  gameType: GameType;
+  checkout: Checkout;
+  randomizePlayerOrder: boolean;
+  disabledStatistics: boolean;
 };
