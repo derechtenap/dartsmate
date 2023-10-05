dfcb000c102d9d78aee28fcd5bd2f252f17e4bfc
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-10-05 17:11:02 +0200

    Simplified function

--- renderer/utils/misc/getUsernameInitials.ts
+++ renderer/utils/misc/getUsernameInitials.ts
@@ -1,31 +1,21 @@
-type Props = {
-  maxInitials?: number;
-  username: string;
-};
-
 /**
  *
  * Get the initials of a username by taking the first character of each word and capitalizing them.
- * If the resulting initials exceed the specified maximum number of characters, only the first 'maxInitials' characters are returned.
+ * If the resulting initials exceed the specified maximum number of characters, only the first 3
+ * characters are returned.
  *
  * @param {string} username - The username from which to generate initials.
- * @param {number} [maxInitials] - The maximum number of characters for the initials. Default is 3.
- * @returns {string} The initials of the username (up to 'maxInitials' characters).
+ * @returns {string} The initials of the username.
  *
  * @example
  * const username = "john doe doe";
  * const initials = getUsernameInitials(username); // Returns "JDD"
- * const limitedInitials = getUsernameInitials(username, 2); // Returns "JD" (limited to 2 characters)
+ *
  */
-export const getUsernameInitials = ({
-  maxInitials,
-  username,
-}: Props): string => {
-  maxInitials ? maxInitials : 3; // Fallback to 3 chars, when maxInitials was not passed
-
+export const getUsernameInitials = (username: string): string => {
   return username
     .split(" ") // Split the username into words
     .map((word) => word.charAt(0).toUpperCase()) // Capitalize the first character of each word
     .join("")
-    .slice(0, maxInitials);
+    .slice(0, 3);
 };
