1199d475eaab922f15a5b8d2200e8a37f557d582
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-10-06 14:01:57 +0200

    Fix: isPlayerBusted not detecting the win condition correctly

--- renderer/utils/match/isPlayerBusted.ts
+++ renderer/utils/match/isPlayerBusted.ts
@@ -2,19 +2,24 @@ import type { Checkout } from "types/match";
 
 /**
  *
- * Checks if the player has busted based on their current score.
- * A player is considered busted if their score is less than or equal to 0.
+ * Checks if the player has busted based on their current score and the chosen checkout.
  *
  * @param {number} currentScore - The player's current score.
  * @param {Checkout} checkout - The type of checkout ("Double" or "Triple").
  * @returns {boolean} `true` if the player has busted, otherwise `false`.
+ *
  */
 export const isPlayerBusted = (
   currentScore: number,
   checkout: Checkout
 ): boolean => {
-  if (checkout === "Double" || checkout === "Triple") {
-    return currentScore <= checkoutValue(checkout);
+  if (currentScore === 0) return false;
+
+  if (checkout === "Double" && currentScore < checkoutValue("Double")) {
+    return true;
+  }
+  if (checkout === "Triple" && currentScore < checkoutValue("Triple")) {
+    return true;
   }
 
   return currentScore < 0;
