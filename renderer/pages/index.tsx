de880b8f773874790c2d48100deb50f120d3e460
Tim <50623315+derechtenap@users.noreply.github.com>
2023-12-15 17:18:29 +0100

    Replaced `Flex` with `Stack` for easier layout styling

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from "next";
-import { Center, Divider, Flex, Title } from "@mantine/core";
+import { Center, Divider, Stack, Title } from "@mantine/core";
 import pkg from "../../package.json";
 import DefaultLayout from "@/components/layouts/Default";
 import { APP_NAME } from "utils/constants";
 
 const IndexPage: NextPage = () => {
+  const APP_VERSION = pkg.version;
+
   // TODO: Currently only shows a logo... Show the user more useful content
   return (
     <DefaultLayout isFetching={false} isLoading={false} isSuccess={true}>
       <Center h="100%">
-        <Flex direction="column">
-          <Title tt="uppercase" fs="italic" size={100} lh={0.6}>
+        <Stack spacing="xs">
+          <Title tt="uppercase" fs="italic" size={100} lh={0.8}>
             {APP_NAME}
           </Title>
-          <Divider my="xs" label={pkg.version} labelPosition="right" />
-        </Flex>
+          <Divider label={APP_VERSION} labelPosition="right" />
+        </Stack>
       </Center>
     </DefaultLayout>
   );
