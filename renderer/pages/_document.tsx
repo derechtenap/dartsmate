5cceb7f93508eefba9ee1e7c1bff36fc1919d302
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-02-11 20:58:55 +0100

    Moved app title to _document file

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { ColorSchemeScript } from "@mantine/core";
 import i18next from "../../next-i18next.config";
+import { APP_NAME } from "@utils/constants";
 
 const Document = () => {
   return (
     <Html lang={i18next.i18n.defaultLocale}>
-      <Head>
+      <Head title={APP_NAME}>
         <ColorSchemeScript defaultColorScheme="auto" />
       </Head>
       <body id="draggable">
