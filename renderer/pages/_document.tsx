095c6a6484fa8898b1a413694cbdd89491745e3e
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-02-20 21:47:27 +0100

    Add resolved lang as html lang attribute

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -1,12 +1,14 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import { ColorSchemeScript } from "@mantine/core";
-
+import { i18n } from "next-i18next";
 import i18next from "../../next-i18next.config.js";
 import { APP_NAME } from "../utils/constants";
 
 const Document = () => {
+  const defaultLocale = i18next.i18n.defaultLocale;
+
   return (
-    <Html lang={i18next.i18n.defaultLocale}>
+    <Html lang={i18n?.resolvedLanguage || defaultLocale}>
       <Head title={APP_NAME}>
         <ColorSchemeScript defaultColorScheme="auto" />
       </Head>
