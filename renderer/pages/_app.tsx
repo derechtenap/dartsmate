c654099e83d72f638253c2882a783affd0b07c7d
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-02-20 21:34:05 +0100

    Add mantine version 7

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,22 +1,18 @@
 import type { AppProps } from "next/app";
-import {
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-} from "@mantine/core";
-import { useHotkeys, useLocalStorage } from "@mantine/hooks";
-// import "../styles/globals.css";
-// import "../styles/scrollbar.css";
+import { MantineProvider } from "@mantine/core";
 import { appWithTranslation } from "next-i18next";
 import { useState } from "react";
-// import { checkAppFolders } from "utils/fs/checkAppFolders";
-// import { useRouter } from "next/router";
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
 import { QueryClient } from "@tanstack/query-core";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { DehydratedState } from "@tanstack/react-query";
-import { Notifications } from "@mantine/notifications";
-// import { APP_NAME } from "utils/constants";
+
+// All packages except `@mantine/hooks` require styles imports!
+import "@mantine/core/styles.css";
+
+// Put overrides with custom stylesheets here
+import "../styles/globals.css";
+import "../styles/scrollbar.css";
 
 const App = ({
   Component,
@@ -24,50 +20,19 @@ const App = ({
 }: AppProps<{
   dehydratedState: DehydratedState;
 }>) => {
-  // const router = useRouter();
   const [queryClient] = useState(() => new QueryClient());
-
-  // Store color scheme in the `localStorage`
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: "mantine-color-scheme",
-    defaultValue: "dark",
-    getInitialValueInEffect: true,
-  });
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-  };
-
-  // Change color scheme by pressing `ctrl + t`
-  useHotkeys([["ctrl+t", () => toggleColorScheme()]]);
-
-  /*
-  useEffect(() => {
-    // Check if all necessary app folders are already created
-    void checkAppFolders();
-  }, [router.pathname]);
- */
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ColorSchemeProvider
-        colorScheme={colorScheme}
-        toggleColorScheme={toggleColorScheme}
+      <MantineProvider
+        theme={{
+          primaryColor: "red",
+        }}
       >
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{
-            colorScheme,
-            primaryColor: "red",
-          }}
-        >
-          <Notifications limit={5} position="top-right" />
-          <Hydrate state={pageProps.dehydratedState}>
-            <Component {...pageProps} />
-          </Hydrate>
-        </MantineProvider>
-      </ColorSchemeProvider>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </MantineProvider>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -1,21 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { createGetInitialProps } from "@mantine/next";
+import { Head, Html, Main, NextScript } from "next/document";
+import { ColorSchemeScript } from "@mantine/core";
+
 import i18next from "../../next-i18next.config.js";
+import { APP_NAME } from "../utils/constants";
 
-const getInitialProps = createGetInitialProps();
+const Document = () => {
+  return (
+    <Html lang={i18next.i18n.defaultLocale}>
+      <Head title={APP_NAME}>
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-
-  render() {
-    return (
-      <Html lang={i18next.i18n.defaultLocale}>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default Document;
