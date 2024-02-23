a3e1dda5cf405dd2c85f68963466d347aea5fbfc
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-02-23 10:13:04 +0100

    Removed testing code

--- renderer/pages/[locale]/index.tsx
+++ renderer/pages/[locale]/index.tsx
@@ -2,32 +2,12 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 
 import { getStaticPaths, makeStaticProperties } from "../../lib/get-static";
-import { Title, Text, Button } from "@mantine/core";
-import { useRouter } from "next/router";
 import DefaultLayout from "@/components/layouts/Default";
 
 const IndexPage = () => {
-  const {
-    i18n: { language: locale },
-    t,
-  } = useTranslation();
-  const router = useRouter();
+  const { t } = useTranslation();
 
-  const changeLocale = (newLocale: string) => {
-    void router.replace("[locale]", newLocale);
-  };
-
-  return (
-    <DefaultLayout withNavbarOpen>
-      <Title>{t("yes")}</Title>
-      <Text>
-        Locale is: <code>{locale}</code>
-      </Text>
-      <Button onClick={() => changeLocale(locale === "en" ? "de" : "en")}>
-        Switch Locale
-      </Button>
-    </DefaultLayout>
-  );
+  return <DefaultLayout withNavbarOpen>Index Route</DefaultLayout>;
 };
 
 export default IndexPage;
