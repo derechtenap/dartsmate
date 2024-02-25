37535586005bea494a43b2c5fd18f58803e872fc
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-02-25 19:22:59 +0100

    Moved stats into component

--- renderer/pages/[locale]/profile/index.tsx
+++ renderer/pages/[locale]/profile/index.tsx
@@ -15,10 +15,28 @@ import {
   Title,
   Tooltip,
 } from "@mantine/core";
+import type { NumberFormatterProps } from "@mantine/core";
 import { IconEdit } from "@tabler/icons-react";
 
 const IndexPage = () => {
   const { t } = useTranslation();
+
+  const stat = (
+    statText: string,
+    value: number,
+    props?: NumberFormatterProps
+  ) => {
+    return (
+      <Stack gap="xs">
+        <Text c="dimmed" fz="xs">
+          {statText}
+        </Text>
+        <Text fw="bold" ff="mono" fz="xl">
+          <NumberFormatter {...props} value={value} />
+        </Text>
+      </Stack>
+    );
+  };
 
   return (
     <DefaultLayout withNavbarOpen>
@@ -34,43 +52,20 @@ const IndexPage = () => {
             </Stack>
             <Group ml="auto">
               <Tooltip label={t("editProfile", { ns: "profile" })} withArrow>
-                <ActionIcon color="white" variant="transparent">
+                <ActionIcon variant="transparent">
                   <IconEdit />
                 </ActionIcon>
               </Tooltip>
             </Group>
           </Group>
           <Group gap="xl" ta="center" grow mt="lg">
-            <Stack gap="xs">
-              <Text c="dimmed" fz="xs">
-                {t("stats.darts")}
-              </Text>
-              <NumberFormatter value={0} />
-            </Stack>
-            <Stack gap="xs">
-              <Text c="dimmed" fz="xs">
-                {t("stats.180s")}
-              </Text>
-              <NumberFormatter value={0} />
-            </Stack>
-            <Stack gap="xs">
-              <Text c="dimmed" fz="xs">
-                {t("stats.matches")}
-              </Text>
-              <NumberFormatter value={0} />
-            </Stack>
-            <Stack gap="xs">
-              <Text c="dimmed" fz="xs">
-                {t("stats.trainings")}
-              </Text>
-              <NumberFormatter value={0} />
-            </Stack>
-            <Stack gap="xs">
-              <Text c="dimmed" fz="xs">
-                {t("stats.avg")}
-              </Text>
-              <NumberFormatter decimalScale={3} value={0} />
-            </Stack>
+            {stat(t("stats.darts"), 0)}
+            {stat(t("stats.180s"), 0)}
+            {stat(t("stats.matches"), 0)}
+            {stat(t("stats.trainings"), 0)}
+            {stat(t("stats.avg"), 0, {
+              decimalScale: 3,
+            })}
           </Group>
         </Stack>
       </Paper>
