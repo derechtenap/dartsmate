d3e6b7de101f96f6ce022ac48bf31d636b069421
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-03-11 12:05:26 +0100

    Improved get preferred locale code

--- main/background.ts
+++ main/background.ts
@@ -4,6 +4,7 @@ import { createWindow } from "./helpers";
 import path from "path";
 import { userStore } from "./helpers/user-store";
 import i18next from "../next-i18next.config.js";
+import log from "electron-log";
 
 export const isProd: boolean = process.env.NODE_ENV === "production";
 
@@ -33,7 +34,27 @@ void (async () => {
     },
   });
 
-  const locale = userStore.get("locale", i18next.i18n.defaultLocale) as string;
+  let preferredLocale = i18next.i18n.defaultLocale;
+
+  /*
+   * Check if the current app locale is included in the supported locales.
+   * This code fallbacks to the i18n `defaultLocale`, if the locale is not
+   * included in the i18n locales array.
+   */
+  if (i18next.i18n.locales.includes(app.getLocale())) {
+    log.info(
+      `User's preferred locale is ${app.getLocale()}. If the user hasn't set a locale already, this locale will be used.`
+    );
+    preferredLocale = app.getLocale().toLowerCase();
+  } else {
+    log.warn(
+      `User's preferred locale is not included in the i18n locales config. Falling back to default locale: ${preferredLocale}!`
+    );
+  }
+
+  // Get stored locale or try to match the client os locale
+  const locale = userStore.get("locale", preferredLocale) as string;
+
   const defaultProfile = userStore.get("defaultProfile", null);
   const port = process.argv[2];
   const profileCreationURL = isProd
