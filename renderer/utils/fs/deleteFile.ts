88f9ae7664d4f709cd7fb1f7f1fd293f44fd4538
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-03-17 16:18:50 +0100

    Removed nextjs routes and config

--- renderer/utils/fs/deleteFile.ts
+++
@@ -1,5 +0,0 @@
-import { unlinkSync } from 'fs'
-
-export const deleteFile = (path: string) => {
-  unlinkSync(path)
-}
--- renderer/utils/fs/readFolder.ts
+++
@@ -1,5 +0,0 @@
-import { readdirSync } from 'fs'
-
-export const readFolder = (path: string) => {
-  return readdirSync(path)
-}
