6240ddaf27bcd80156e20b73570ceb558807af25
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-10-10 14:53:53 +0200

    Add create header greeting

--- renderer/pages/[locale]/index.tsx
+++ renderer/pages/[locale]/index.tsx
@@ -1,26 +1,19 @@
-import { useTranslation } from "next-i18next";
+// import { useTranslation } from "next-i18next";
 import { getStaticPaths, makeStaticProperties } from "@/lib/getStatic";
 import DefaultLayout from "@/components/layouts/Default";
-import { Box, Stack, Title } from "@mantine/core";
-import DarkenedText from "@/components/content/DarkenedText";
+import { Stack } from "@mantine/core";
 import HorizontalScrollContainer from "@/components/content/horizontalScroll/HorizontalScrollContainer";
 import { useDefaultProfile } from "hooks/useDefaultProfile";
+import HeaderGreeting from "@/components/HeaderGreeting";
 
 const IndexPage = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   const { data: defaultProfile } = useDefaultProfile();
 
   return (
     <DefaultLayout withNavbarOpen={false}>
-      <Stack>
-        {defaultProfile && (
-          <Box component="header">
-            <Title fz="h1">
-              {t("greetingText", { FIRST_NAME: defaultProfile.name.firstName })}
-            </Title>
-            <DarkenedText>{t("greetingSubtext")}</DarkenedText>
-          </Box>
-        )}
+      <Stack my="lg">
+        <HeaderGreeting firstName={defaultProfile?.name.firstName} />
         <HorizontalScrollContainer></HorizontalScrollContainer>
       </Stack>
     </DefaultLayout>
