c2cc0f3f0a8cda95962ee3c939fbd92b4a6799ba
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-10-17 15:32:45 +0200

    Add get preferred locale helper function

--- main/background.ts
+++ main/background.ts
@@ -2,10 +2,10 @@ import { app } from "electron";
 import serve from "electron-serve";
 import { createWindow } from "./helpers";
 import path from "path";
-import i18next from "../next-i18next.config.js";
 import log from "electron-log";
 import { appSettingsStore, profilesStore } from "./helpers/stores";
 import logSystemInfo from "./helpers/utils/logSystemInfo";
+import getPreferredLocale from "./helpers/utils/getPreferredLocale";
 
 export const isProd: boolean = process.env.NODE_ENV === "production";
 
@@ -48,25 +48,8 @@ void (async () => {
     },
   });
 
-  let preferredLocale = i18next.i18n.defaultLocale;
-
-  /*
-   * Check if the current app locale is included in the supported locales.
-   * This code fallbacks to the i18n `defaultLocale`, if the locale is not
-   * included in the i18n locales array.
-   */
-  if (i18next.i18n.locales.includes(app.getLocale())) {
-    log.info(
-      `User's preferred locale is ${app.getLocale()}. If the user hasn't set a locale already, this locale will be used.`
-    );
-    preferredLocale = app.getLocale().toLowerCase();
-  } else {
-    log.warn(
-      `User's preferred locale is not included in the i18n locales config. Falling back to default locale: ${preferredLocale}!`
-    );
-  }
-
-  // Get stored locale or try to match the client os locale
+  // Retrieve the stored locale from app settings, or use the client's preferred locale
+  const preferredLocale = getPreferredLocale();
   const locale = appSettingsStore.get("locale", preferredLocale);
   const defaultProfile = profilesStore.get("defaultProfile");
 
