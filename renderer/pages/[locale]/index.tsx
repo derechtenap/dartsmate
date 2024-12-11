e90250b0e5f795159ed1e36710fe94df0481e753
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-12-11 16:18:52 +0100

    Show greeting on index page

--- renderer/pages/[locale]/index.tsx
+++ renderer/pages/[locale]/index.tsx
@@ -1,18 +1,17 @@
-// import { useTranslation } from "next-i18next";
 import { getStaticPaths, makeStaticProperties } from "@/lib/getStatic";
 import DefaultLayout from "@/components/layouts/Default";
 import { Stack } from "@mantine/core";
 import HorizontalScrollContainer from "@/components/content/horizontalScroll/HorizontalScrollContainer";
-// import HeaderGreeting from "@/components/HeaderGreeting";
+import HeaderGreeting from "@/components/HeaderGreeting";
+import useDefaultProfile from "hooks/getDefaultProfile";
 
 const IndexPage = () => {
-  // const { t } = useTranslation();
-  // const { data: defaultProfile } = useDefaultProfile();
+  const defaultProfile = useDefaultProfile();
 
   return (
     <DefaultLayout withNavbarOpen>
       <Stack my="lg">
-        {/*<HeaderGreeting firstName={defaultProfile?.name.firstName} />*/}
+        <HeaderGreeting firstName={defaultProfile?.name.firstName || ""} />
         <HorizontalScrollContainer></HorizontalScrollContainer>
       </Stack>
     </DefaultLayout>
